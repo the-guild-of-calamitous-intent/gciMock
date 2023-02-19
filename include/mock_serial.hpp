59a64fe0b25bc0ea0eeb4769608f8432058b4a6e
walchko <walchko@users.noreply.github.com>
2023-02-19 14:34:53 -0700

    more methods

--- include/mock_serial.hpp
+++ include/mock_serial.hpp
@@ -17,9 +17,13 @@ struct Stream {
   void println(string a) { cout << a << endl; }
   void setTimeout(int) {}
   int available() { return 1; }
-  int read() { return 1; }
-  int write(uint8_t *, int sz) { return sz; }
+  int peak() { return 100; }
+  int read() { return 100; }
+  string readString() { return "dummy"; }
+  int readBytes(uint8_t *buf, int size) { return size; };
+  int write(uint8_t *, int size) { return size; }
   int write(uint8_t) { return 1; }
+  int write(const string& s) { return s.length(); }
 
   // this isn't real, so always return "true"
   inline explicit operator bool() const noexcept { return true; }
